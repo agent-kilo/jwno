573eff4a7e1701a3ef8682bfff4d4f66bfabac3e
k <>
2024-06-08 18:34:55 +0000

    Wrap console init code
    FossilOrigin-Name: 722d60b0e8ad295f9d48ed34b17f7ba126f72ef86197cbdae363ebacade8cc0a

--- c/winmain_stub.c
+++ c/winmain_stub.c
@@ -6,6 +6,35 @@
 int main(int argc, const char **argv);
 
 
+int early_console_alloc(void) {
+    if (!AllocConsole()) {
+        return -1;
+    }
+
+    errno_t err = 0;
+    FILE *con_stdin;
+    FILE *con_stdout;
+    FILE *con_stderr;
+
+    err = freopen_s(&con_stdin, "CONIN$", "r", stdin);
+    if (err) {
+        return err;
+    }
+
+    err = freopen_s(&con_stdout, "CONOUT$", "w", stdout);
+    if (err) {
+        return err;
+    }
+
+    err = freopen_s(&con_stderr, "CONOUT$", "w", stderr);
+    if (err) {
+        return err;
+    }
+
+    return 0;
+}
+
+
 int APIENTRY WinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
@@ -16,14 +45,13 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance,
     (void)lpCmdLine;
     (void)nCmdShow;
 
-    AllocConsole();
-
-    FILE *con_stdin;
-    FILE *con_stderr;
-    FILE *con_stdout;
-    freopen_s(&con_stdin, "CONIN$", "r", stdin);
-    freopen_s(&con_stdout, "CONOUT$", "w", stdout);
-    freopen_s(&con_stderr, "CONOUT$", "w", stderr);
+    int err = early_console_alloc();
+    if (err) {
+        char msg_buf[128];
+        sprintf(msg_buf, "early_console_alloc() failed: %d", err);
+        MessageBox(NULL, msg_buf, "Error", MB_ICONEXCLAMATION | MB_OK);
+        return 1;
+    }
 
     return main(__argc, __argv);
 }
