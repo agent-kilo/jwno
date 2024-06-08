e47a8d3666c15c7d455d31ebc7e1ad7ea0d8653f
k <>
2024-06-08 18:17:57 +0000

    Remove redundant pragma comment
    FossilOrigin-Name: 6d994c4417251a91dd97f3821375a518f2b8fb246444e6316e9dd53dcb68dbfb

--- c/winmain_stub.c
+++ c/winmain_stub.c
@@ -3,12 +3,6 @@
 #include <windows.h>
 
 
-#pragma comment(lib,"comctl32.lib")    
-#pragma comment(linker,"\"/manifestdependency:type='win32' \
-name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
-processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
-
-
 int main(int argc, const char **argv);
 
 
